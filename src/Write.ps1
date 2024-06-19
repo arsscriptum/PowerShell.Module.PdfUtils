fbb9acc36b6e13b94dfd45ae0239cf9c78a8ca4a
Guillaume Plante <guillaume.plante@luminator.com>
2024-06-19 18:19:10 -0400

    auto

--- src/Write.ps1
+++ src/Write.ps1
@@ -15,16 +15,19 @@ function Write-PdfCryptoFile {
         [Parameter(Mandatory=$true,Position=0)]
         [string]$Path,
         [Parameter(Mandatory=$true,Position=1)]
+        [string]$Title,
+        [Parameter(Mandatory=$true,Position=2)]
         [string]$Text
     )
     try{
         Register-ITextSharpLib
+        
         $pdf = [iTextSharp.text.Document]::new()
-        $Null = New-PdfDocument -Document $pdf -File "$Path" -TopMargin 20 -BottomMargin 20 -LeftMargin 20 -RightMargin 20 -Author "Guillaume Plante"
+        $Null = New-PdfDocument -Document $pdf -File "$Path" -TopMargin 20 -BottomMargin 20 -LeftMargin 20 -RightMargin 20 -Author "$ENV:USERNAME"
         $pdf.Open()
       
-        $ret = Add-TitleToPdf -Document $pdf -Text "Crypto Data" -Color "Red" -Centered
-        $ret = Add-TextToPdf -Document $pdf -Text "$Text"
+        $ret = Add-TitleToPdf -Document $pdf -Text "$Title" -Color "Red" -Centered -FontName "Consolas"
+        $ret = Add-TextToPdf -Document $pdf -Text "$Text" -FontName "Consolas"
     
         $pdf.Close()
     }
@@ -33,31 +36,6 @@ function Write-PdfCryptoFile {
     }
 }
 
-
-function Write-PdfFile {
-
-    [CmdletBinding(SupportsShouldProcess)]
-    param(
-        [Parameter(Mandatory=$true,Position=0)]
-        [string]$Path,
-        [Parameter(Mandatory=$true,Position=1)]
-        [string]$Text
-    )
-    try{
-        Register-ITextSharpLib
-        $pdf = [iTextSharp.text.Document]::new()
-        New-PdfDocument -Document $pdf -File "$Path" -TopMargin 20 -BottomMargin 20 -LeftMargin 20 -RightMargin 20 -Author "Guillaume Plante"
-        $pdf.Open()
-      
-        Add-TitleToPdf -Document $pdf -Text "This Is the Title Test" -Color "Blue" -Centered
-        Add-TextToPdf -Document $pdf -Text "$Text"
-    
-        $pdf.Close()
-    }
-    catch{
-        Write-Error $_
-    }
-}
 
 Function New-PdfDocument{
     [CmdletBinding(SupportsShouldProcess)]
@@ -78,13 +56,19 @@ Function New-PdfDocument{
         [Parameter(Mandatory=$false)]
         [string]$Author = "$ENV:USERNAME"
     )
-    
-    $Document.SetPageSize([iTextSharp.text.PageSize]::A4)
-    $Document.SetMargins($LeftMargin, $RightMargin, $TopMargin, $BottomMargin)
-    [void][iTextSharp.text.pdf.PdfWriter]::GetInstance($Document, [System.IO.File]::Create($File))
-    $Document.AddAuthor($Author)
-    $Document;
+    try{
+        $Document.SetPageSize([iTextSharp.text.PageSize]::A4)
+        $Document.SetMargins($LeftMargin, $RightMargin, $TopMargin, $BottomMargin)
+        [void][iTextSharp.text.pdf.PdfWriter]::GetInstance($Document, [System.IO.File]::Create($File))
+        $Document.AddAuthor($Author)
+        $Document;
+    }catch{
+        Show-ExceptionDetails $_ -ShowStack
+    }
 }
+
+
+
         
 # Add a text paragraph to the document, optionally with a font name, size and color
 function Add-TextToPdf {
@@ -100,15 +84,23 @@ function Add-TextToPdf {
         [Parameter(Mandatory=$false)]
         [int32]$FontSize = 12, 
         [Parameter(Mandatory=$false)]
-        [string]$Color = "BLACK"
+        [ValidateSet("WHITE", "LIGHT_GRAY", "GRAY", "DARK_GRAY", "BLACK", "RED", "PINK", "ORANGE", "YELLOW", "GREEN", "MAGENTA", "CYAN", "BLUE")]
+        [string]$Color = "BLACK",
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('BOLDITALIC', 'DEFAULTSIZE', 'ITALIC', 'NORMAL', 'STRIKETHRU', 'UNDEFINED', 'UNDERLINE')]
+        [string]$Style = "NORMAL"
     )
-
-    $p = New-Object iTextSharp.text.Paragraph
-    $p.Font = [iTextSharp.text.FontFactory]::GetFont($FontName, $FontSize, [iTextSharp.text.Font]::NORMAL, [iTextSharp.text.BaseColor]::$Color)
-    $p.SpacingBefore = 2
-    $p.SpacingAfter = 2
-    $p.Add($Text)
-    $Document.Add($p)
+    try{
+       
+        $p = New-Object iTextSharp.text.Paragraph
+        $p.Font = [iTextSharp.text.FontFactory]::GetFont($FontName, $FontSize,[iTextSharp.text.Font]::"$Style", [iTextSharp.text.BaseColor]::"$Color")
+        $p.SpacingBefore = 2
+        $p.SpacingAfter = 2
+        $p.Add($Text)
+        $Document.Add($p)
+    }catch{
+        Show-ExceptionDetails $_ -ShowStack
+    }
 }
         
 # Add a title to the document, optionally with a font name, size, color and centered
@@ -124,18 +116,26 @@ function Add-TitleToPdf {
         [string]$FontName = "Arial", 
         [Parameter(Mandatory=$false)]
         [int32]$FontSize = 12, 
+        [ValidateSet("WHITE", "LIGHT_GRAY", "GRAY", "DARK_GRAY", "BLACK", "RED", "PINK", "ORANGE", "YELLOW", "GREEN", "MAGENTA", "CYAN", "BLUE")]
+        [string]$Color = "BLACK",
         [Parameter(Mandatory=$false)]
-        [string]$Color = "BLACK", 
+        [ValidateSet('BOLDITALIC', 'DEFAULTSIZE', 'ITALIC', 'NORMAL', 'STRIKETHRU', 'UNDEFINED', 'UNDERLINE')]
+        [string]$Style = "NORMAL",
         [Parameter(Mandatory=$false)]
         [switch]$Centered
     )    
-    $p = New-Object iTextSharp.text.Paragraph
-    $p.Font = [iTextSharp.text.FontFactory]::GetFont($FontName, $FontSize, [iTextSharp.text.Font]::BOLD, [iTextSharp.text.BaseColor]::$Color)
-    if ($Centered) { $p.Alignment = [iTextSharp.text.Element]::ALIGN_CENTER }
-    $p.SpacingBefore = 5
-    $p.SpacingAfter = 5
-    $p.Add($Text)
-    $Document.Add($p)
+
+    try{
+        $p = New-Object iTextSharp.text.Paragraph
+        $p.Font = [iTextSharp.text.FontFactory]::GetFont($FontName, $FontSize,[iTextSharp.text.Font]::"$Style", [iTextSharp.text.BaseColor]::"$Color")
+        if ($Centered) { $p.Alignment = [iTextSharp.text.Element]::ALIGN_CENTER }
+        $p.SpacingBefore = 5
+        $p.SpacingAfter = 5
+        $p.Add($Text)
+        $Document.Add($p)
+    }catch{
+        Show-ExceptionDetails $_ -ShowStack
+    }        
 }
         
 # Add an image to the document, optionally scaled
@@ -151,10 +151,14 @@ function Add-ImageToPdf {
         [int32]$Scale = 100
     )
 
-    [iTextSharp.text.Image]$img = [iTextSharp.text.Image]::GetInstance($File)
-    if ($Centered) { $p.Alignment = [iTextSharp.text.Element]::ALIGN_CENTER }
-    $img.ScalePercent(100)
-    $Document.Add($img)
+    try{
+        [iTextSharp.text.Image]$img = [iTextSharp.text.Image]::GetInstance($File)
+        if ($Centered) { $p.Alignment = [iTextSharp.text.Element]::ALIGN_CENTER }
+        $img.ScalePercent(100)
+        $Document.Add($img)
+    }catch{
+        Show-ExceptionDetails $_ -ShowStack
+    }
 }
         
 # Add a table to the document with an array as the data, a number of columns, and optionally centered
@@ -170,29 +174,17 @@ function Add-TableToPdf {
         [Parameter(Mandatory=$false)]
         [switch]$Centered
     ) 
-
-    $t = New-Object iTextSharp.text.pdf.PDFPTable($Cols)
-    $t.SpacingBefore = 5
-    $t.SpacingAfter = 5
-    if (!$Centered) { $t.HorizontalAlignment = 0 }
-    foreach ($data in $Dataset)
-    {
-        $t.AddCell($data);
+    try{
+        $t = New-Object iTextSharp.text.pdf.PDFPTable($Cols)
+        $t.SpacingBefore = 5
+        $t.SpacingAfter = 5
+        if (!$Centered) { $t.HorizontalAlignment = 0 }
+        foreach ($data in $Dataset)
+        {
+            $t.AddCell($data);
+        }
+        $Document.Add($t)
+    }catch{
+        Show-ExceptionDetails $_ -ShowStack
     }
-    $Document.Add($t)
-}
-
-function Test-CreatePdf{
-        
-    Register-ITextSharpLib
-    $pdf = [iTextSharp.text.Document]::new()
-    New-PdfDocument -Document $pdf -File "C:\Tmp\Test.pdf" -TopMargin 20 -BottomMargin 20 -LeftMargin 20 -RightMargin 20 -Author "Guillaume Plante"
-    $pdf.Open()
-    # Add-Image -Document $pdf -File "$Flogo" -Centered
-    $Ret = Add-TitleToPdf -Document $pdf -Text "THIS IS THE STORY OF THE LONELY VAGABOND" -Color "Magenta" -Centered
-    $Ret = Add-TextToPdf -Document $pdf -Text "This would serve as a short paragraph,This would serve as a short paragraph,This would serve as a short paragraph,This would serve as a short paragraph"
-    #Add-Table -Document $pdf -Dataset @('Name', "$first", "Login", "$userprinc", "Email", "$SamAccountName", "Password", "String") -Cols 2 -Centered
-    Add-TextToPdf -Document $pdf -Text "We could use this space to show the Help Desk Ticket System"
-    $pdf.Close()
-
 }
